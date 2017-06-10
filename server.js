121ac6f49756423a91d4fce276f515ba514346fc
velascoDev <dracontium2@gmail.com>
2017-06-09 23:49:17 -0500

    Added basic design

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.render("main");
 });
 
 app.listen(port, () => {
