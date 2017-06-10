b1475355776dd2c933efdc16a83000ccc10de461
velascoDev <dracontium2@gmail.com>
2017-06-10 14:33:14 -0500

    Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const importCss = require("gulp-import-css");
 const cleanCSS = require("gulp-clean-css");
 const livereload = require("gulp-livereload");
 
-gulp.task("js", function() {
+gulp.task("js", () => {
     return (
         browserify("./public/js/Nimble.js")
         .transform("babelify", {presets: ["es2015"]})
@@ -17,17 +17,17 @@ gulp.task("js", function() {
     );
 });
 
-gulp.task("pug-1", function(){
+gulp.task("pug-1", () => {
     gulp.src("./views/*.pug")
         .pipe(livereload());
 });
 
-gulp.task("pug-2", function(){
+gulp.task("pug-2", () => {
     gulp.src("./views/partials/*.pug")
         .pipe(livereload());
 });
 
-gulp.task("css", function () {
+gulp.task("css", () => {
     gulp.src("./public/css/Nimble.css")
         .pipe(importCss())
         .pipe(cleanCSS({compatibility: "ie8"}))
@@ -35,7 +35,7 @@ gulp.task("css", function () {
         .pipe(livereload());
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", () => {
       livereload.listen();
       gulp.watch("./views/*.pug", ["pug-1"]);
       gulp.watch("./views/partials/*.pug", ["pug-2"]);
