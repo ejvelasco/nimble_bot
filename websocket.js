b318cda5d308e759ace22ec06f1d0890407fa904
velascoDev <dracontium2@gmail.com>
2017-06-10 14:28:41 -0500

    Cleaned up websocket code

--- websocket.js
+++ websocket.js
@@ -1,62 +1,56 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+"use strict";
 
-var server = http.createServer(function(request, response) {
-  console.log('Received request from ' + request.url);
-  response.writeHead(404);
-  response.end();
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+const port = 1337;
+const server = http.createServer((request, response) => {
+	console.log(`Received request from ${request.url}`);
+	response.writeHead(404);
+	response.end();
+});
+const wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false 
 });
 
-server.listen(1337, function() {
-    console.log('Server is listening on port 1337.');
+server.listen(port, () => {
+	console.log(`Server is listening on ${port} 1337.`);
 });
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false // because security matters
+wsServer.on('request', (request) => {
+	const connection = isAllowedOrigin(request.origin) ? request.accept('echo-protocol', request.origin) : request.reject();
+	//message event
+	connection.on('message', (msg) => {
+		const message = msg.utf8Data;
+		if(message === "test"){
+			connection.sendUTF('Hello, I am Nimble.');
+		} else {  
+			connection.sendUTF('Received Message: ' + message);
+		}
+		console.log('Received Message: ' + message );
+	});
+	//close event
+	connection.on('close', (reasonCode, description) => {
+			console.log(connection.remoteAddress + ' has been disconnected.');
+	}); 
 });
 
 function isAllowedOrigin(origin) {
-  console.log('Connection requested from origin ' + origin);
-
-  valid_origins = [
-    'http://localhost:5000',
-    '127.0.0.1',
-    'null'
-  ];
-
-  if (valid_origins.indexOf(origin) != -1) {
-    console.log('Connection accepted from origin ' + origin);
-    return true;
-  }
-
-  console.log('Origin ' + origin + ' is not allowed.')
-  return false;
+	//valid
+	const validOrigins = [
+		'http://localhost:5000',
+		'127.0.0.1',
+		'null'
+	];
+	//connection requested
+	console.log(`Connection requested from origin ${origin}`);
+	//allowed
+	if (validOrigins.indexOf(origin) != -1) {
+		console.log(`Connection accepted from origin ${origin}`);
+		return true;
+	}
+	//not allowed
+	console.log(`Origin ${origin} is not allowed.`);
+	return false;
 }
-
-
-wsServer.on('connect', function(webSocketConnection) {
-// webSocketConnection.sendUTF("Hello, I am Nimble.");
-});
-
-wsServer.on('request', function(request) {
-
-  var connection = isAllowedOrigin(request.origin) ?
-    request.accept('echo-protocol', request.origin)
-    : request.reject();
-
-  connection.on('message', function(msg) {
-    const message = msg.utf8Data;
-    if(message === "test"){
-      connection.sendUTF('Hello, I am Nimble.');
-    } else {  
-      connection.sendUTF('Received Message: ' + message);
-    }
-    console.log('Received Message: ' + message );
-    
-    
-  });
-  connection.on('close', function(reasonCode, description) {
-      console.log(connection.remoteAddress + ' has been disconnected.');
-  });
-});
